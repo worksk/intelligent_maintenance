bd77b06aab6aa74db98e8587c87db861ed63ef35
Wyxwx <791989352@qq.com>
2019-07-22 14:38:23 +0800

    初始化

---
+++ src/main/java/com/monitoring/intelligent_maintenance/IntelligentMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.monitoring.intelligent_maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentMaintenanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentMaintenanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/monitoring/intelligent_maintenance/IntelligentMaintenanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.monitoring.intelligent_maintenance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntelligentMaintenanceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
